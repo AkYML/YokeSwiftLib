32948bd450c880085570c31dca8cc639bd5da8d5
AkYML <102035914+AkYML@users.noreply.github.com>
2022-04-07 05:28:09 +0530

    update lib

--- Yoke.xcframework/ios-x86_64-simulator/Yoke.framework/Headers/Yoke.h
+++ Yoke.xcframework/ios-x86_64-simulator/Yoke.framework/Headers/Yoke.h
@@ -141,6 +141,14 @@ __attribute__((swift_name("KotlinBoolean")))
 @end;
 
 __attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("ApplicationApi")))
+@interface YokeApplicationApi : YokeBase
+- (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
++ (instancetype)new __attribute__((availability(swift, unavailable, message="use object initializers instead")));
+- (void)aboutCallback:(void (^)(NSString *))callback __attribute__((swift_name("about(callback:)")));
+@end;
+
+__attribute__((objc_subclassing_restricted))
 __attribute__((swift_name("Greeting")))
 @interface YokeGreeting : YokeBase
 - (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
